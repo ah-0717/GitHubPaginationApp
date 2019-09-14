122a66a8c516cc00ec1a9d7814d2442bc99417da
ah <ah@dummy.com>
2019-09-15 01:30:17 +0900

    setting env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { environment } from './../environments/environment';
+
+// console.log(environment.GITHUB_TOKEN);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'GitHubPaginationApp';
+
 }
